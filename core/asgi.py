5a55c4876aed0b6510ee1dfdfef8d9a46d2a4450
Yannick Oe <yannickoetelshoven@gmail.com>
2026-08-07 20:09:30 +0200

    style: trim generated boilerplate docstrings

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,9 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
+"""ASGI entry point for the Videoflix backend."""
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_asgi_application()
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+"""WSGI entry point for the Videoflix backend."""
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
